a883b7cd9cb2304431aa599537feb976a04ba039
Sergio Alvarez <salvarez.casais@gmail.com>
2018-01-30 18:35:05 +0100

    First model Devices

--- xesteq/models.py
+++ xesteq/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class DeviceType(models.Model):
+
+    dtype = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.type
+        
+        
+class Location(models.Model):
+
+    localization = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.localizacion
+        
+            
+class Department(models.Model):
+
+    department = models.CharField(max_length=30)     
+
+    def __str__(self):
+        return self.area
+        
+        
+class DeviceData(models.Model):
+
+    fk_typeDev = models.ForeignKey('DeviceType')
+    brand = models.CharField(max_length=30)
+    model = models.CharField(max_length=30)
+    serialno = models.CharField(max_length=30)
+    mac = models.CharField(max_length=30, null=True)
+    so = models.CharField(max_length=30, null=True)
+    sokey = models.CharField(max_length=30, null=True)
+    domain = models.CharField(max_length=30, null=True)
+    devname = models.CharField(max_length=30)
+    devuser = models.CharField(max_length=30, null=True)
+    fk_localization =models.ForeignKey('Location')
+    fk_depart = models.ForeignKey('Department')
+    ts_Create = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.brand
